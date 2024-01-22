379f8edfd55b298819550301ce334956071d5442
athanasiia <grvkan@gmail.com>
2024-01-22 14:31:17 +0300

    feat: complete files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const promises = require("fs/promises");
+const fs = require("fs");
+
+const files = promises.readdir(path.join(__dirname, "secret-folder"), {withFileTypes: true});
+
+files.then((readingResult) => {
+  for (res of readingResult) {
+    if (res.isFile()) {
+      let parsedName = path.parse(res.name);
+      fs.stat(path.join(__dirname, "secret-folder", res.name), (err, stats) => {
+        console.log(`${parsedName.name} - ${path.extname(parsedName.base)} - ${stats.size} bytes`);
+      });
+    }
+  }
+});
